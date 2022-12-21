5f523acf36b3afead664e31e75a2d5828b4763c0
Jon Wire <iambipedal@gmail.com>
2022-12-20 18:33:58 -0600

    rename output to compile.js, compile from file arg instead of stdin

--- src/compile.ts
+++ src/compile.ts
@@ -1,8 +1,9 @@
+import process from 'process';
 import { readFileSync } from 'fs';
 import MonkeyScript from './monkeyscript-lang';
 
 const ast = MonkeyScript.parse({
-	code: readFileSync(0, 'utf-8').trim()
+	code: readFileSync(process.argv[2], 'utf-8').trim()
 });
 
 console.dir(ast, { depth: 2 });
